d512a1c8758de06f42c084147723f8c969928bd3
Andres Soto <84928376+asotito@users.noreply.github.com>
2025-06-09 22:42:13 -0500

    feat: created project

---
+++ TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/TesisAoskauntoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesisaoskaunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisAoskauntoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisAoskauntoApplication.class, args);
+    }
+
+}
---
+++ TesisAoskaunto/src/test/java/com/example/tesisaoskaunto/TesisAoskauntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesisaoskaunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisAoskauntoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
