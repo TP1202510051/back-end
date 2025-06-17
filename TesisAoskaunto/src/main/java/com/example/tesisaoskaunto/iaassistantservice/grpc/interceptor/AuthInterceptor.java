b07d654f2c04918f08174e376957dd7796c04cd9
Andres Soto <84928376+asotito@users.noreply.github.com>
2025-06-17 00:25:17 -0500

    feat: created message service and class to get messages by conversation id, w

--- TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/iaassistantservice/grpc/interceptor/AuthInterceptor.java
+++
@@ -1,4 +0,0 @@
-package com.example.tesisaoskaunto.iaassistantservice.grpc.interceptor;
-
-public class AuthInterceptor {
-}
---
+++ TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/messagesservice/infrastructure/repositories/MessagesRepository.java
@@ -0,0 +1,10 @@
+package com.example.tesisaoskaunto.messagesservice.infrastructure.repositories;
+
+import com.example.tesisaoskaunto.iaassistantservice.domain.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessagesRepository extends JpaRepository<Message, Long> {
+    List<Message> findByConversationId(String conversationId);
+}
