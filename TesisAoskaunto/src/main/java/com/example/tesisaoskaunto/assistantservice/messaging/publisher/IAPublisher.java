689f8876dd7bda9eb1103200a20443a98070a879
Andres Soto <84928376+asotito@users.noreply.github.com>
2025-06-25 17:21:17 -0500

    feat: added websocket configuration to manipulate incoming messages from the ia to send them in real time to the front

--- TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/assistantservice/messaging/publisher/IAPublisher.java
+++ TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/assistantservice/messaging/publisher/IAPublisher.java
@@ -1,11 +1,24 @@
 package com.example.tesisaoskaunto.assistantservice.messaging.publisher;
 
 import org.springframework.stereotype.Service;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
 
 @Service
 public class IAPublisher {
 
-    public void publishResponse(String message, Long conversationId) {
-        System.out.println("Lo que se va a enviar en el websocket: " + message + " " + conversationId);
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public IAPublisher(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void publishResponse(String message, Long projectId) {
+        //System.out.println("Lo que se va a enviar en el websocket: " + message + " " + projectId);
+
+        messagingTemplate.convertAndSend(
+                "/topic/conversation/" + projectId,
+                message
+        );
     }
 }
