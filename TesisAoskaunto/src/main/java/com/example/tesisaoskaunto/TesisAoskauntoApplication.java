30824cb4af28c72b8247a7702b93570ee96f2902
Andres Soto <84928376+asotito@users.noreply.github.com>
2025-07-07 17:01:46 -0500

    feat: changed credentials

--- TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/TesisAoskauntoApplication.java
+++ TesisAoskaunto/src/main/java/com/example/tesisaoskaunto/TesisAoskauntoApplication.java
@@ -7,44 +7,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import io.swagger.v3.oas.annotations.info.Info;
 
-import java.io.File;
-import java.io.FileWriter;
-
 
 @SpringBootApplication
 @EnableJpaAuditing
 @OpenAPIDefinition(info = @Info(title = "Mi API", version = "v1", description = "Documentación de mi servicio"))
 public class TesisAoskauntoApplication {
 
-
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure()
                 .ignoreIfMissing()
                 .load();
-
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         System.setProperty("PROJECT_ID", dotenv.get("PROJECT_ID"));
         System.setProperty("TOPIC-PROMPT", dotenv.get("TOPIC-PROMPT"));
         System.setProperty("SUBSCRIPTION_PROMPT", dotenv.get("SUBSCRIPTION_PROMPT"));
         System.setProperty("FUNCION_RECEIVER", dotenv.get("FUNCION_RECEIVER"));
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", dotenv.get("GOOGLE_APPLICATION_CREDENTIALS"));
         System.setProperty("IA_TOKEN", dotenv.get("IA_TOKEN"));
         System.setProperty("API_POST_URL", dotenv.get("API_POST_URL"));
 
-        String credentialsJson = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
-        if (credentialsJson != null && !credentialsJson.isEmpty()) {
-            try {
-                File tempFile = File.createTempFile("gcp-creds", ".json");
-                tempFile.deleteOnExit();
-                try (FileWriter writer = new FileWriter(tempFile)) {
-                    writer.write(credentialsJson);
-                }
-                System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", tempFile.getAbsolutePath());
-            } catch (Exception e) {
-                throw new RuntimeException("Error al crear archivo temporal de credenciales GCP", e);
-            }
-        }
-
         SpringApplication.run(TesisAoskauntoApplication.class, args);
+
     }
+
+
 }
